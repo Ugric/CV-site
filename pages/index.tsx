20b760bc24673c559437e0d187ef9a0e86aae0ba
W̷̛͒i̶̓͗l̷͗̌ľ̶͘ <62452284+Ugric@users.noreply.github.com>
2022-05-15 21:05:10 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,8 @@ import type { NextPage } from 'next'
 import wave from './images/wave.png'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
-import { useState } from 'react'
-import { Controller, Effect } from "./components/controller";
 
 const Home: NextPage = () => {
-  const [myval, setval] = useState(0)
   return (
     <>
       <div className={styles.top_container}>
