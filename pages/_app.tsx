73f174f1daaa183e95f92fcbe91f102f9a168e00
William Bell <62452284+Ugric@users.noreply.github.com>
2022-05-24 08:14:03 +0000

    SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,11 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>William Bell - wbell.dev</title>
-        <meta charset="UTF-8">
-        <meta name="description" content="Hi 👋, I'm Will. I'm passionate about developing software.">
+        <meta charset="UTF-8" />
+        <meta name="description" content="Hi 👋, I'm Will. I'm passionate about developing software." />
         <meta name="keywords" content="Will, William Bell, Will Bell, William Bell github, Will Bell github, Will Bell CV">
-        <meta name="author" content="William Bell">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="William Bell" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
     </>
