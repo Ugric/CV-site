364ee06d1e08c9344dc17158daaf30e07ba12cda
William Bell <epicugric@gmail.com>
2022-12-12 09:57:30 +0000

    fix reverse animation on timeline

--- pages/components/timeline-points.tsx
+++ pages/components/timeline-points.tsx
@@ -3,36 +3,36 @@ import { ReactNode } from "react";
 import styles from "/styles/timeline.module.css";
 
 const Timeline = (props: { Timeline: ReactNode[]; [key: string]: any }) => {
-  return (
-    <div
-      {...(() => {
-        const copy: Record<string, any> = Object.assign({}, props);
-        delete copy.Timeline;
-        return copy;
-      })()}
-      className={styles.Timeline}
-    >
-      {props.Timeline?.map((value, index) => (
-        <TimelinePoint
-          text={value}
-          key={index}
-          delay={0.25 * (props.Timeline.length - index - 1) + 1.25}
-        ></TimelinePoint>
-      ))}
-    </div>
-  );
+    return (
+        <div
+            {...(() => {
+                const copy: Record<string, any> = Object.assign({}, props);
+                delete copy.Timeline;
+                return copy;
+            })()}
+            className={styles.Timeline}
+        >
+            {props.Timeline?.map((value, index) => (
+                <TimelinePoint
+                    text={value}
+                    key={index}
+                    delay={0.25 * index + 2}
+                ></TimelinePoint>
+            ))}
+        </div>
+    );
 };
 
 const TimelinePoint = ({ text, delay }: { text: ReactNode; delay: number }) => {
-  return (
-    <div className={styles.point}>
-      <div className={styles.line}></div>
-      <div
-        className={styles.circle}
-        style={{ animationDelay: delay + "s" }}
-      ></div>
-      <div className={styles.text}>{text}</div>
-    </div>
-  );
+    return (
+        <div className={styles.point}>
+            <div className={styles.line}></div>
+            <div
+                className={styles.circle}
+                style={{ animationDelay: delay + "s" }}
+            ></div>
+            <div className={styles.text}>{text}</div>
+        </div>
+    );
 };
 export default Timeline;
